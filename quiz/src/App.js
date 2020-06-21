2695ec44a261f0d65c55f51b1d5a0d77b2209b3f
Hemanth Kumar <56812697+kumar342@users.noreply.github.com>
2020-06-21 10:30:50 +0530

    quiz result

---
+++ quiz/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Create, Timer } from "./Components/Quiz";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://opentdb.com/api.php?amount=10")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ categories: data });
+      })
+      .catch((error) => console.error(error));
+  }
+  render() {
+    let { categories } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Quiz App</h1>
+        </header>
+
+        {<Create categories={categories} />}
+      </div>
+    );
+  }
+}
+
+export default App;
